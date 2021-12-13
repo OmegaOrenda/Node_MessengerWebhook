7e518eca17329dd48a408cfdbfaec51d071b1ad8
Kartikey Gupta <kartikeygupta@Kartikeys-MacBook-Air.local>
2021-12-13 17:15:03 +0530

    update

--- index.js
+++ index.js
@@ -65,7 +65,6 @@ function handleMessage(sender_psid, received_message) {
 
   // Check if the message contains text
   if (received_message.text) {    
-console.log('**************************************'+ received_message.text);
     // Create the payload for a basic text message
     response = {
       "text": `You sent the message: "${received_message.text}". Now send me an image!`
