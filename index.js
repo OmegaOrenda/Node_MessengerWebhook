2694a8b16452edd22b0a2f37fd5d980baefb8333
Kartikey Gupta <kartikeygupta@Kartikeys-MacBook-Air.local>
2021-12-13 16:43:40 +0530

    update

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ function callSendAPI(sender_psid, response) {
     },
     "message": response
   }
-console.log('**************************************');
+
   // Send the HTTP request to the Messenger Platform
   request({
     "uri": "https://graph.facebook.com/v2.6/me/messages",
@@ -102,6 +102,8 @@ console.log('**************************************');
   }, (err, res, body) => {
     if (!err) {
       console.log('message sent!!')
+      console.log('**************************************'+ body);
+      console.log('**************************************1'+ res);
     } else {
       console.error("Unable to send message:" + err);
     }
